99ad7ae80ef420ef6c44419b541c811c6ba8f96d
inshady <inshadyprod@gmail.com>
2025-12-02 09:38:21 +0300

    p_t

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
-struct p_t {
-  int x, y;
-};
+namespace topit {
+  struct p_t {
+    int x, y;
+  };
+  bool operator==(p_t a, p_t b);
+  bool operator!=(p_t a, p_t b);
+}
+
 
 int main() {
-    
+  using namespace topit;
+  p_t a{1,1}, b{0,1};
+  std::cout << (a==b) << "\n";
+}
+
+bool topit::operator==(p_t a, p_t b) {
+  return a.x == b.x && a.y == b.y;
+}
+
+bool topit::operator!=(p_t a, p_t b) {
+  return !(a==b);
 }
