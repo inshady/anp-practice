cdaa9befe5c67ce979419c761f59ca4035ff88cc
inshady <inshadyprod@gmail.com>
2025-12-02 10:27:14 +0300

    ready to write program

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 namespace topit {
   struct p_t {
@@ -20,6 +21,7 @@ namespace topit {
   };
 
   struct Dot: IDraw {
+    explicit Dot(p_t dd);
     p_t begin() const override;
     p_t next(p_t prev) const override;
     p_t d;
@@ -29,9 +31,24 @@ namespace topit {
 
 int main() {
   using namespace topit;
-  p_t a{1,1}, b{0,1};
-  std::cout << (a==b) << "\n";
+  int err = 0;
+  IDraw* shp[3] = {};
+  try {
+    shp[0] = new Dot({0, 0});
+    shp[1] = new Dot({2, 3});
+
+  } catch (...) {
+    err = 1;
+  }
+  delete shp[0];
+  delete shp[1];
+  return err;
 }
+
+topit::Dot::Dot(p_t dd):
+ IDraw(),
+ d{dd}
+{}
 
 topit::p_t topit::Dot::begin() const {
   return d;
